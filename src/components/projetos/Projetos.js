6e09fa98a8a8fd4baadfce44e6ea3b3af26cd862
Lihsolive <liliamoliveira13@gmail.com>
2023-05-28 22:13:40 -0300

    Criando componente ProjetosCard

--- src/components/projetos/Projetos.js
+++ src/components/projetos/Projetos.js
@@ -1,14 +1,13 @@
 import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
-// import { ProjectCard } from "./ProjectCard";
+import { ProjetosCard } from "../projetos-card/ProjetosCard";
 import projImg1 from "../../assets/img/project-img1.png";
 import projImg2 from "../../assets/img/project-img2.png";
 import projImg3 from "../../assets/img/project-img3.png";
-// import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
+import colorSharp2 from "../../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
-import "./Projetos.css"
-
+import "./Projetos.css";
 
 export const Projetos = () => {
   const projetos = [
@@ -47,46 +46,65 @@ export const Projetos = () => {
     <section className="projeto" id="projeto">
       <Container>
         <Row>
-          <Col>
-            <h2>Projetos</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-            <Tab.Container id="projetos-tabs" defaultActiveKey="first">
-            <Nav variant="pills" defaultActiveKey="/home">
-              <Nav.Item>
-                <Nav.Link eventKey="first" >Tab One</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab Two</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">
-                  Tab Three
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-            <Tab.Content>
-                <Tab.Pane eventKey="first">
-                    <Row>
-                        {
-                            projetos.map((projeto, index) => {
-                                return (
-                                    <p>{projeto.title}</p>
-                                )
-                            })
-                        }
-                    </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
-                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
-
-            </Tab.Content>
-            </Tab.Container>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projetos</h2>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry.
+                  </p>
+                  <Tab.Container id="projetos-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justfy-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab One</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projetos.map((project, index) => {
+                            return <ProjetosCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
+      <img className="background-image-right" alt="" src={colorSharp2}></img>
     </section>
   );
 };
