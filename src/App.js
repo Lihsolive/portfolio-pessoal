aec060ca8e3cf25f115e4c4e7fe9168dee7b8e50
Lihsolive <liliamoliveira13@gmail.com>
2023-06-16 19:17:06 -0300

    Criando o componente Timeline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { NavBar } from "./components/navbar/NavBar";
 import { Banner } from "./components/banner/Banner";
-import { Tech } from "./components/tech/Tech"
+import { Timeline } from "./components/timeline/Timeline";
+import { Tech } from "./components/tech/Tech";
 import { Projetos } from "./components/projetos/Projetos";
 import { Contato } from "./components/contato/Contato";
 import { Footer } from "./components/footer/Footer";
@@ -17,6 +18,7 @@ function App() {
       <ToastContainer style={{ position: "fixed" }} />
       <NavBar />
       <Banner />
+      <Timeline />
       <Tech />
       <Projetos />
       <Contato />
